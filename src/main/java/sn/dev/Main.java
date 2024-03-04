d41a973b8eb0798d97cf993e2da4284b2e3dd294
diadhiouababacarpro@gmail.com <diadhiouababacarpro@gmail.com>
2024-03-03 19:23:14 -1100

    trainDevops_start

---
+++ src/main/java/sn/dev/Main.java
@@ -0,0 +1,17 @@
+package sn.dev;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Entrée with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("===========================================================");
+        System.out.printf("                VOTRE COMPTEUR JUSQU'A 10                   ");
+        System.out.println("===========================================================");
+        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(""+i);
+        }
+    }
+}
